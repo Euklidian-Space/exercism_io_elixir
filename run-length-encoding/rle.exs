dd9521b62841257e0063d66d47485b522f1115e3
Gabriel <gabriel.grover@aofl.com>
2017-08-21 02:26:19 -0700

    encode tests passing

--- run-length-encoding/rle.exs
+++ run-length-encoding/rle.exs
@@ -7,12 +7,41 @@ defmodule RunLengthEncoder do
   "2A3B4C" => "AABBBCCCC"
   """
   @spec encode(String.t) :: String.t
+  def encode(""), do: ""
   def encode(string) do
-
+    string
+    |> do_encode("", 0, "")
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
 
   end
+  
+  @spec do_encode(String.t, String.t, number, String.t) :: String.t
+  defp do_encode("", res, count, curr) do 
+    cond do 
+      count > 1 -> "#{res}#{count}#{curr}" 
+
+      true -> "#{res}#{curr}"
+    end 
+  end 
+  defp do_encode(<< head :: binary - 1, tail :: binary >>, res, count, "") do 
+    do_encode(tail, res, count + 1, head)  
+  end
+  defp do_encode(<< head :: binary - 1, tail :: binary >> = str, res, count, curr) do 
+    cond do 
+      curr == head -> do_encode(tail, res, count + 1, curr)
+
+      count > 1 -> do_encode(str, "#{res}#{count}#{curr}", 0, "")
+
+      true -> do_encode(str, "#{res}#{curr}", 0, "")
+    end 
+  end 
 end
+
+
+
+
+
+
